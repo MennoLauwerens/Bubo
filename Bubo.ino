9150df692fc744ce0c4b85c113e9dbb101509b89
Menno Lauwerens <github@lauwerens.com>
2017-01-06 13:26:46 +0100

    Update Bubo.ino
    Added base for commandqueue

--- Bubo.ino
+++ Bubo.ino
@@ -8,6 +8,13 @@
 #include <Adafruit_NeoPixel.h>
 #include <Servo.h>
 
+#define EyesLedAction 1
+#define EyesRotAction 2
+#define WingAction 3
+#define NeckAction 4
+#define LegsAction 5
+#define Mpu6050Action 6
+
 #define MaxQueueLength 25
 unsigned long QueueTime[MaxQueueLength];
 int QueueModule[MaxQueueLength];
@@ -27,12 +34,58 @@ void setup() {
 }
 
 void loop() { 
+   if (QueueChanged) {
+    //Serial.print("QueueLength: ");
+    //Serial.println(QueueLength);
+    //DumpQueue();
+    QueueChanged=false;
+  }
   do_eyes_led();
   do_eyes_rot();
   do_wings();
   do_neck();
   do_legs();
   //read_mpu6050();
+  for (int i=0;i < QueueLength;i++) {
+
+    if (QueueTime[i] <= millis()) {
+      int QModule = QueueModule[i];
+      int QAction = QueueAction[i];
+      int QData = QueueData[i];
+      for (int j=i;j < QueueLength;j++) {
+        QueueTime[j] = QueueTime[j+1];
+        QueueModule[j] = QueueModule[j+1];
+        QueueAction[j] = QueueAction[j+1];        
+        QueueData[j] = QueueData[j+1];
+      }
+      QueueLength--;
+      QueueChanged=true; 
+      switch (QModule) {
+        case 0:
+          // do nothing for debugging
+          break;
+        case EyesLedAction:
+          //do_eyes_led(QAction);
+          break;
+        case EyesRotAction:
+          //do_eyes_rot(QAction);
+          break;   
+        case WingAction:
+          //init_wings(QAction);
+          break;
+        case NeckAction:
+          //do_neck(QAction);
+          break;
+        case LegsAction:  
+          //do_legs(QAction,QData);
+          break;
+        case Mpu6050Action:
+          //read_mpu6050(QAction,QData);
+          break;
+      }
+      break;
+    }
+  }
 }
 
 // Add event to comandqueue
