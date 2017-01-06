6a7171409dc9b00544fcc2a375299670a4277ab7
Menno Lauwerens <github@lauwerens.com>
2017-01-06 13:13:58 +0100

    Update Legs.ino

--- Legs.ino
+++ Legs.ino
@@ -6,9 +6,13 @@
 
 Servo ServoLegs;
 
-void init_wings() {
+void init_legs() {
   ServoLegs.attach(LegPin);  
   ServoLegs.write(LegNeutralPos);
   //Queue(500,EarAction,0);
   //Queue(3000,EarAction,2);
+}
+
+void do_legs(){
+  ServoLegs.write(LegNeutralPos+10);
 }
