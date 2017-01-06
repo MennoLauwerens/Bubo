242a8d70ed8086a24a7ecc902dad95335ff9bbc1
Menno Lauwerens <github@lauwerens.com>
2017-01-06 13:08:38 +0100

    Update Neopix.ino

--- Neopix.ino
+++ Neopix.ino
@@ -4,14 +4,14 @@
 
 Adafruit_NeoPixel EyePixels = Adafruit_NeoPixel(LedCount, LedPin, NEO_GRB + NEO_KHZ800);
 
-void init_eyes() {
+void init_eyes_led() {
   EyePixels.begin();
   for(int i=0; i<= LedCount-1; i++) EyePixels.setPixelColor(i , 0);
   EyePixels.show();
   //Queue(2000,EyeAction,20);
 }
 
-void do_eyes(){
+void do_eyes_led(){
   EyePixels.setPixelColor(0 , 255 , 0   , 0);
   EyePixels.setPixelColor(1 , 0   , 255 , 0);
   EyePixels.show();
